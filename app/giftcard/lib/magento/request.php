f1d57ea6b286de71673bf1e1bdf10033dca7ab85
ZorroZJR <jinrong.zhang@d1m.cn>
2017-11-27 23:25:47 +0800

    file_get_content换成curl

--- app/giftcard/lib/magento/request.php
+++ app/giftcard/lib/magento/request.php
@@ -8,8 +8,16 @@ class giftcard_magento_request{
 	
 	public function post($post){
 		$url=$this->arrSetting['magento_url'].$post;
-		$output=file_get_contents($url);
+		$ch = curl_init();//初始化一个cURL会话
+		curl_setopt($ch, CURLOPT_URL,$url);
+		//curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0');
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+		curl_setopt($ch, CURLOPT_HEADER,0);
+		$output = curl_exec($ch);
 		$result=json_decode($output,true);
+		
 		return $result;
 	}
 	
