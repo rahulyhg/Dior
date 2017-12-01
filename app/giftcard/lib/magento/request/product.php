93e8d08fd1e185663b188bd8a2779e503a223d8a
ZorroZJR <jinrong.zhang@d1m.cn>
2017-12-01 13:05:07 +0800

    接口修改

--- app/giftcard/lib/magento/request/product.php
+++ app/giftcard/lib/magento/request/product.php
@@ -2,7 +2,7 @@
 class giftcard_magento_request_product extends giftcard_magento_request
 {	
 	public function getProduct($card_id){
-		$result=$this->post("index/info/wechat_card_id/".$card_id);
+		$result=$this->post("index/info/type/gift/wechat_card_id/".$card_id);
 		if($result['success']=="1"&&!empty($result['cord_info']['cord_products'][0])){
 			return $result['cord_info']['cord_products'];
 		}
