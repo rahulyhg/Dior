76a2befd784a88e4b268e5364f1651dcd8e7489a
payne.wu <payne.wu@d1m.cn>
2018-06-06 12:13:23 +0800

    !#LVMH-70发货信息CARD明细信息修改

--- app/qmwms/lib/request/qimen.php
+++ app/qmwms/lib/request/qimen.php
@@ -276,7 +276,7 @@ class qmwms_request_qimen{
                 'retailPrice'     =>'0.00',//零售价(零售价=实际成交价+单件商品折扣金额) (取值不确定)
                 'actualPrice'     => '0.00', //必须 实际成交价
                 'discountAmount'  =>'0.00',//单件商品折扣金额
-                'extendProps'     =>array('itemType'=>'GIFT WRAP','itemMessage'=>'MCD package'),
+                'extendProps'     =>array('itemType'=>'GIFT WRAP'),
             );
             $body['orderLines']['orderLine'][] = $mcdPackage;
         }
@@ -295,7 +295,7 @@ class qmwms_request_qimen{
                 'retailPrice'     =>'0.00',//零售价(零售价=实际成交价+单件商品折扣金额) (取值不确定)
                 'actualPrice'     => '0.00', //必须 实际成交价
                 'discountAmount'  =>'0.00',//单件商品折扣金额
-                'extendProps'     =>array('itemType'=>'Card','itemMessage'=>'WelcomeCard'),
+                'extendProps'     =>array('itemType'=>'Card'),
             );
             $body['orderLines']['orderLine'][] = $welcomeCard;
         }
