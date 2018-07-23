91f6096d2bf80e4389232d9463cb3e78787adc59
payne.wu <payne.wu@d1m.cn>
2018-07-23 20:24:55 +0800

    +#LVMH81 记录报警邮件发送的文件日志。看邮件有没有发送成功

--- app/qmwms/controller/service.php
+++ app/qmwms/controller/service.php
@@ -10,8 +10,8 @@ class qmwms_ctl_service{
 
         $content = file_get_contents('php://input');
         $params = $_REQUEST;
-        error_log(date('Y-m-d H:i:s').'##########XML文件信息##########'."\r\n".var_export($content,true)."\r\n", 3, ROOT_DIR.'/data/logs/wmstoms'.date('Y-m-d').'.xml');
-        error_log(date('Y-m-d H:i:s').'##########HEADER参数信息##########'."\r\n".var_export($params,true)."\r\n", 3, ROOT_DIR.'/data/logs/wmstoms'.date('Y-m-d').'.xml');
+        //error_log(date('Y-m-d H:i:s').'##########XML文件信息##########'."\r\n".var_export($content,true)."\r\n", 3, ROOT_DIR.'/data/logs/wmstoms'.date('Y-m-d').'.xml');
+        //error_log(date('Y-m-d H:i:s').'##########HEADER参数信息##########'."\r\n".var_export($params,true)."\r\n", 3, ROOT_DIR.'/data/logs/wmstoms'.date('Y-m-d').'.xml');
         $res = kernel::single('qmwms_response_qmoms')->qmToOms($content,$params);
         echo $res;
     } 
