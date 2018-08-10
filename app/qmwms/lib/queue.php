aef928471176cdf1e06381de3f719ba3bff18ca8
ZorroZJR <jinrong.zhang@d1m.cn>
2018-08-10 17:28:40 +0800

    v1.0

---
+++ app/qmwms/lib/queue.php
@@ -0,0 +1,58 @@
+<?php
+class qmwms_queue{
+    
+    private $_queue;
+    
+    private $_method = array(
+        'deliveryOrderConfirm' => 'do_delivery',
+        'returnOrderConfirm' => 'do_finish',
+    );
+    
+    public function __construct(&$app)
+    {
+        $this->_queue = app::get('qmwms')->model('queue');
+    }
+    
+    public function doQueue()
+    {
+        $datas = $this->_queue->getList('*', array('status'=>0));
+        if(empty($datas)) {
+            return true;
+        }
+        
+        $objQm = kernel::single('qmwms_response_qmoms');
+        
+        foreach($datas as $data) {
+            $id = $data['id'];
+            $method = $data['api_method'];
+            
+            $this->begin($id);
+            
+            try{
+                if(! $objQm->{$this->_method[$method]}($data['api_params'])) {
+                    $this->end($id, 2);    
+                    continue;
+                }
+                $this->end($id, 3); 
+            }catch(Exception $e) {
+                $this->end($id, 2, $e->getMessage());
+            }
+        }
+    }
+    
+    public function begin($id) 
+    {
+        $this->_queue->update(array('status'=>1), array('id'=>$id));
+    }
+    
+    public function end($id, $status='2', $msg='') 
+    {
+        if($status == '2') {
+            $this->_queue->update(array('status'=>2, 'msg'=>$msg), array('id'=>$id));
+        }else{
+            $this->_queue->delete(array('id'=>$id));
+        }
+    }
+    
+}
+?>
