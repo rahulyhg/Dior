eba3fec59ae6329cb95515e9cf44d44660e99601
yaowenzhu-august <august.yao@d1m.cn>
2018-08-24 11:06:45 +0800

    log日志倒叙返回

---
+++ app/ome/model/kafka/api/log.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: august.yao
+ * Date: 2018/08/15
+ * Time: 14:46
+ */
+class ome_mdl_kafka_api_log extends dbeav_model
+{
+    var $defaultOrder = array('api_request_time', ' DESC');
+}
