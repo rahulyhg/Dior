36c14df5b2e3de7d1954a82a3b65af4c805585fe
李俊 <jun.li@d1m.cn>
2019-02-19 16:17:22 +0800

    Bug-LVMH-178 订单状态会写失败问题修复

--- app/omemagento/lib/service/order.php
+++ app/omemagento/lib/service/order.php
@@ -13,7 +13,27 @@ class omemagento_service_order{
 	}
 
 	public  function update_status($order_bn,$status,$tracking_code='',$event_time='',$reship_items=''){
+        $productMdl = app::get('ome')->model('products');
+        $orderMdl   = app::get('ome')->model('orders');
+        $orderItemMdl = app::get('ome')->model('order_items');
+        $orderInfo = $orderMdl->getList('ship_status,order_id',array('order_bn'=>$order_bn));
+        if($orderInfo[0]['ship_status']=='0'&&$status='refunding'&&$reship_items){
+            $orderItems = $orderItemsMdl->getList('*',array('order_id'=>$orderInfo[0]['order_id']));
+            $reship_items = array();
+            foreach($orderItems as $oitem){
+                $reship_items[] = array(
+                        'sku'=>$oitem['bn'],
+                        'nums'=>$oitem['nums'],
+                        'price'=>$oitem['price'],
+                        'oms_rma_id'=>0,//始终用新reship_id
+                    );
+            }
+        }
 		if($reship_items){
+            foreach($reship_items as $key=>$item){
+              $productInfo = $productMdl->getList('short_bn',array('bn'=>$item['sku']));
+              $reship_items[$key]['short_sku'] = $productInfo[0]['short_bn'];
+            }
 			$params = array('order_id'=>$order_bn,'status'=>$status,'tracking_code'=>$tracking_code,'event_time'=>date('Y-m-d H:i:s',time()),'refund_info'=>$reship_items);
 		}else{
 			if(!$event_time){
@@ -26,6 +46,7 @@ class omemagento_service_order{
 			$params['is_refund_new'] = 1;
 		}
 		if($status=='refunding'||$status=='refund_required'){
+            
 			$params['is_refund_new'] = 0;
 		}
 		$this->request->do_request('order',$params);
